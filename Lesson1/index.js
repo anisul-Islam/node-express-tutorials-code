4e2434a698feda239bd9a27e256ce06b7d13974d
Anisul Islam <anisul.islam@tuni.fi>
2020-10-28 20:31:09 +0200

    Local module, exports, require

---
+++ Lesson1/index.js
@@ -0,0 +1,3 @@
+const {getName} = require('./student');
+
+console.log(getName());
---
+++ Lesson1/student.js
@@ -0,0 +1,19 @@
+exports.getName = () =>{
+    return "Anisul Islam";
+}
+
+const getAge = () => {
+    return "25";
+}
+
+const cgpa = 3.92;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
